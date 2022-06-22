e0a1e3f167ff26cc6037da009228f6ade92bfdb9
Mat Jaggard <mat@billforward.net>
2022-06-22 12:37:41 +0100

    Update to 9.5

--- create.sh
+++ create.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker build . --tag billforwardadmin/sonarqube-billforward:9.3.0.51899
-docker push billforwardadmin/sonarqube-billforward:9.3.0.51899
+docker build . --tag billforwardadmin/sonarqube-billforward:9.5.0.56709
+docker push billforwardadmin/sonarqube-billforward:9.5.0.56709
